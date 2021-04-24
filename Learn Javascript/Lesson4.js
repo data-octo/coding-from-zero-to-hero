71144f581c12faa6c1def72f336367f6f0d20410
Data Octo <data-octo@users.noreply.github.com>
2021-04-23 17:48:34 -0700

    20210423

---
+++ Learn Javascript/Lesson4.js
@@ -0,0 +1,26 @@
+var winston = {
+  age: 19,
+  eyes: "black",
+  likes: ["programming", "being programmed"],
+  isCool: true,
+  birthplace: {
+    city: "Mountain View",
+    state: "California",
+  },
+};
+
+fill(255, 255, 255);
+
+textSize(16);
+text("All about Winston:", 10, 30);
+text("Winston is " + winston.age + " years old", 10, 50);
+text("Winston has " + winston.eyes + " eyes", 10, 70);
+text("Winston likes " + winston.likes[0] + " and " + winston.likes[1], 10, 90);
+text(
+  "Winston was born in " +
+    winston.birthplace.city +
+    ", " +
+    winston.birthplace.state,
+  10,
+  110
+);
---
+++ Learn Javascript/README.md
@@ -0,0 +1,5 @@
+
+### Prerequisites
+
+#### Install VSCode extension: p5canvas
+The p5canvas extensions allows you to preview your p5js code in a canvas side by side with your code. The canvas refreshes live, while editing.
